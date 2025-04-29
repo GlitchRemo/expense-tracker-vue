c40ab3b96a881839c2a8b647f3cd9197dfbb3858
glitch_remo <riya.ghosal@thoughtworks.com>
2025-04-29 21:17:37 +0530

    add create db query

--- backend/database/db.go
+++ backend/database/db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jmoiron/sqlx"
@@ -10,9 +11,7 @@ import (
 var db *sqlx.DB
 
 func InitDB() {
-	log.Println("Initializing database connection...")
 	var err error
-	log.Println("Initializing database connection...")
 	connStr := "host=172.17.0.1 user=riya password=riyaghosalrg@123 dbname=myappdb sslmode=disable"
 	db, err = sqlx.Open("postgres", connStr)
 	if err != nil {
@@ -23,7 +22,53 @@ func InitDB() {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
+	err = createUsersTable()
+	if err != nil {
+		log.Fatal("error while creating the users table")
+	}
+
+	err = createExpensesTable()
+	if err != nil {
+		log.Fatal("error while creating the expenses table")
+	}
+
 	log.Println("Database connection established")
+}
+
+func createUsersTable() error {
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(50) NOT NULL,
+			total_income FLOAT DEFAULT 0,
+			password VARCHAR(50) NOT NULL
+		)
+	`
+
+	_, err := db.Exec(createTableQuery)
+	if err != nil {
+		return fmt.Errorf("error creating table %v", err)
+	}
+
+	return nil
+}
+func createExpensesTable() error {
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS expenses (
+			id SERIAL PRIMARY KEY,
+			category VARCHAR(50) NOT NULL,
+			create_date DATE DEFAULT CURRENT_DATE,
+			note VARCHAR(50) NOT NULL,
+			FOREIGN KEY (user_id) REFERENCES users(id),
+		)
+	`
+
+	_, err := db.Exec(createTableQuery)
+	if err != nil {
+		return fmt.Errorf("error creating table %v", err)
+	}
+
+	return nil
 }
 
 func CloseDB() {
